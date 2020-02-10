a325dabac1129c863049a899add275e66f87d17c
tazo90 <tazo90@gmail.com>
2020-02-10 14:54:33 +0100

    Add model field to Sentiment domain

--- app/models/domain/sentiment.py
+++ app/models/domain/sentiment.py
@@ -4,3 +4,4 @@ from pydantic import BaseModel, Field
 class Sentiment(BaseModel):
     text: str = Field(..., min_length=3, max_length=1000)
     language: str = Field(..., min_length=2, max_length=3)
+    model: str = Field(..., min_length=3, max_length=50)
